5f41799f11ff1afdabaf9fd101b4064137f05389
hiroshi nagao <hi_1205@hotmail.com>
2024-03-28 20:57:57 +0900

    add event model

---
+++ db/migrate/20240328115630_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[7.1]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :description
+      t.string :location
+      t.date :date
+      t.time :start_time
+      t.time :finish_time
+
+      t.timestamps
+    end
+  end
+end
