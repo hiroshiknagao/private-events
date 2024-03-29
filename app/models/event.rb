8b665d9176404a7bf357cf2b089283a2c7127eb1
hiroshi nagao <hi_1205@hotmail.com>
2024-03-29 09:56:12 +0900

    Add devise, association/validation to event/user models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :date, presence: true
+
+  belongs_to :creator, class_name: "User"
 end
